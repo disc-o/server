e1c4351b043f61479fa6021deaaea65dc141b1b4
Tan Yuanhong <leotan.yh@gmail.com>
2019-05-27 00:05:30 +0800

    prototype

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
 const express = require('express')
+const request = require('request')
 const app = express()
-const port = 3000
+var bodyParser = require('body-parser')
+const port = 3001
+
+const nanoid = require('nanoid')
+
+var clients = new Map()
+var users = new Map()
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(bodyParser.json())
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/uid', (req, res) => {
+  var uid = nanoid(8);
+  clients[uid] = req.query.callback
+  console.log(clients[uid])
+  res.send({ uid: uid });
+})
+
+app.post('/uid', (req, res) => {
+  var uid = req.body.uid
+  if (clients[uid] == undefined) {
+    res.status(400).send('failed')
+  } else {
+    var options = {
+      uri: clients[uid],
+      method: 'POST',
+      json: true,
+      body: {
+        'proxy_url': req.body.proxy_url
+      }
+    }
+    request(options, (err, resq, body) {
+
+    })
+    res.send('success');
+  }
+})
+
